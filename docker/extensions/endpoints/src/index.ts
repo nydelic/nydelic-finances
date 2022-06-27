792ab9acacc16491ee2ac3ac389206d56dc34744
josias-r <josias.ribi@gmail.com>
2022-06-27 21:24:48 +0200

    fix: start working on webhook endpoint

--- docker/extensions/endpoints/src/index.ts
+++ docker/extensions/endpoints/src/index.ts
@@ -1,5 +1,59 @@
 import { defineEndpoint } from "@directus/extensions-sdk";
+import { hmacValidator } from "@adyen/api-library";
+import HttpRequestError from "@nydelic/toolbox/dist/handlers/http/HttpRequestError";
+import httpErrorResponse from "@nydelic/toolbox/dist/handlers/http/httpErrorResponse";
+import httpResponse from "@nydelic/toolbox/dist/handlers/http/httpResponse";
+import testRequests from "./testRequests";
+
+const ADYEN_HMAC_KEY = process.env.ADYEN_HMAC_KEY;
+if (!ADYEN_HMAC_KEY) {
+  throw new Error("Missing HMAC key fro Adyen");
+}
+
+const validator = new hmacValidator();
+
+// Notification Request JSON
+const notificationRequest = {
+  live: "false",
+  notificationItems: [
+    {
+      NotificationRequestItem: testRequests.myNotificationRequestItem,
+    },
+  ],
+} as const;
+const notificationRequestItems = notificationRequest.notificationItems;
 
 export default defineEndpoint((router) => {
-  router.post("/", (_req, res) => res.json({ message: "Hello, World!.-.." }));
+  router.post("/", (_req, res) => {
+    try {
+      // Handling multiple notificationRequests
+      notificationRequestItems.forEach(function (notificationRequestItem) {
+        // Handle the notification
+        if (
+          validator.validateHMAC(
+            notificationRequestItem.NotificationRequestItem,
+            ADYEN_HMAC_KEY
+          )
+        ) {
+          // Process the notification based on the eventCode
+          const eventCode =
+            notificationRequestItem.NotificationRequestItem.eventCode;
+
+          console.log(eventCode);
+        } else {
+          throw new HttpRequestError(
+            "EUNAUTHORIZED",
+            403,
+            "Unauthorized request"
+          );
+        }
+      });
+
+      return httpResponse(res, 200, "Sücksess", {
+        key: process.env.ADYEN_HMAC_KEY,
+      });
+    } catch (error) {
+      return httpErrorResponse(res, error);
+    }
+  });
 });
---
+++ docker/extensions/endpoints/src/testRequests.ts
@@ -0,0 +1,32 @@
+import { NotificationRequestItem } from "@adyen/api-library/lib/src/typings/notification/notificationRequestItem";
+
+const myNotificationRequestItem: NotificationRequestItem = {
+  additionalData: {
+    expiryDate: "12/2012",
+    authCode: "1234",
+    cardSummary: "7777",
+    // totalFraudScore: "10",
+    hmacSignature: "EhsiNpf4PQ7zmAVG2rjQ348+5Yq0iv61a+0iYhLov/E=",
+    // NAME2: "VALUE2",
+    // NAME1: "VALUE1",
+    // "fraudCheck-6-ShopperIpUsage": "10",
+  },
+  amount: { currency: "GBP", value: 20100 },
+  eventCode: NotificationRequestItem.EventCodeEnum["Authorisation"],
+  eventDate: "2022-06-27T20:23:00+02:00",
+  merchantAccountCode: "NydelicECOM",
+  merchantReference: "8313842560770001",
+  operations: [
+    NotificationRequestItem.OperationsEnum["Cancel"],
+    NotificationRequestItem.OperationsEnum["Capture"],
+    NotificationRequestItem.OperationsEnum["Refund"],
+  ],
+  paymentMethod: "visa",
+  pspReference: "DTOXILNPTUKY7OKL",
+  reason: "1234:7777:12/2012",
+  success: NotificationRequestItem.SuccessEnum["True"],
+};
+
+const testRequests = { myNotificationRequestItem };
+
+export default testRequests;
